378cdc5ab5585636bc14090dfa6a20134ac6394b
Steve Mills <stevepatrickmills@gmail.com>
2017-01-29 17:15:44 -0600

    Initialize repo

---
+++ public/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	res.send('Hello express!');
+});
+
+app.get('/about', function (req, res) {
+	res.send('About Us!');
+
+});
+
+app.listen(3000, function () {
+	console.log('Express server started!');
+});
+
+
